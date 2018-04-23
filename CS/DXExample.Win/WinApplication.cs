2533c53159cee6cf5b5bdae4d8af3409181dbba6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXExample.Win/WinApplication.cs
@@ -0,0 +1,23 @@
+using DevExpress.ExpressApp.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp;
+
+namespace DXExample.Win {
+    public partial class DXExampleWindowsFormsApplication : WinApplication {
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(args.ConnectionString, args.Connection);
+        }
+        public DXExampleWindowsFormsApplication() {
+            InitializeComponent();
+        }
+
+        private void DXExampleWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+    }
+}
